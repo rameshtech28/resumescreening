4cc56f8a1b5b592a959b12dd368d3bab6114a409
Krishna Sahu <88479371+KrishnaSahuTechment@users.noreply.github.com>
2024-07-24 11:32:38 +0530

    Icon changed

--- pages/Chatbot_FAQ.py
+++ pages/Chatbot_FAQ.py
@@ -71,7 +71,7 @@ config = {
 
 
 st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.sidebar.page_link("pages/Job Postings Authoring.py", label="Job Postings Authoring", icon="📑")
+st.sidebar.page_link("pages/Job Postings Authoring.py", label="Job Postings Authoring", icon="💼")
 st.sidebar.page_link("pages/Smart_ATS.py", label="Smart ATS", icon="📑")
 st.sidebar.page_link("pages/Chatbot.py", label="Chatbot", icon="🤖")
 st.sidebar.page_link("pages/Chatbot_FAQ.py", label="Help Desk", icon="📜")
